74ce923a3f7a5cf046a500bb9874b8b3e96100ae
b45i <amalshajan2011@gmail.com>
2021-04-04 18:58:50 +0530

    resposinve layout

---
+++ index.js
@@ -0,0 +1,17 @@
+// const btn = document.querySelector('action-btn');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const btn = document.querySelector('#actionBtn');
+
+//     setInterval(() => {
+//         [
+//             'CAD/CAM Engineer',
+//             'Product Designer',
+//             'Design Generalist',
+//             'UI/UX Designer',
+//         ].forEach(t => {
+//             btn.innerText = t;
+//             setTimeout(_ => _, 1000);
+//         });
+//     }, 1000);
+// });
