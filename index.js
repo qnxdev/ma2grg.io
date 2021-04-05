0b0daf59fbc30dab5f04e018fb17e096d84ca7f2
b45i <amalshajan2011@gmail.com>
2021-04-05 19:46:05 +0530

    btn animation

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-// const btn = document.querySelector('action-btn');
+const CHANGE_DELAY = 4000;
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const btn = document.querySelector('#actionBtn');
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('#actionBtn');
 
-//     setInterval(() => {
-//         [
-//             'CAD/CAM Engineer',
-//             'Product Designer',
-//             'Design Generalist',
-//             'UI/UX Designer',
-//         ].forEach(t => {
-//             btn.innerText = t;
-//             setTimeout(_ => _, 1000);
-//         });
-//     }, 1000);
-// });
+    let i = 0;
+    const texts = [
+        'CAD/CAM Engineer',
+        'Product Designer',
+        'Design Generalist',
+        'UI/UX Designer',
+    ];
+
+    const updateText = () => {
+        btn.classList.add('fadeOut');
+        btn.classList.remove('fadeIn');
+        setTimeout(_ => {
+            btn.classList.remove('fadeOut');
+            i = (i + 1) % texts.length;
+            btn.innerText = texts[i];
+            btn.classList.add('fadeIn');
+        }, 1000);
+    };
+
+    let timer = setInterval(updateText, CHANGE_DELAY);
+
+    btn.addEventListener('click', _ => {
+        updateText();
+        clearInterval(timer);
+        timer = setInterval(updateText, CHANGE_DELAY);
+    });
+});
